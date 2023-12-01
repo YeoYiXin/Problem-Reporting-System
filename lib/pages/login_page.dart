294085dfe5f6dd0ddba206536a51eca56b53d84e
Shenfung <sam.chinsf@gmail.com>
2023-12-01 19:08:10 +0800

    Upgraded gradle, improved login page, setup settings page and made other improvements

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:problem_reporting_system/services/my_textfield.dart';
-import 'package:problem_reporting_system/services/my_button.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -10,98 +11,187 @@ class LoginPage extends StatelessWidget {
   final passwordController = TextEditingController();
 
   // sign user in method
-  void signUserIn() {
-    // Implement your sign-in logic here
+  void signUserIn(BuildContext context) {
+    Navigator.pushNamed(context, '/homepage');
+  }
+
+  void registerUser(BuildContext context) {
+    Navigator.pushNamed(context, '/registrationpage');
+  }
+
+  Future<void> showSignInDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // title:
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0), // Adjust the border radius as needed
+          ),
+          content: Container(
+            width: MediaQuery.of(context).size.width * 0.8, // Adjust the width as needed
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Center(child: Text('Sign In',style: TextStyle(fontSize: 34,fontWeight: FontWeight.w700,),)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text('Access to the fixes on campus. Report faulty facilities by just taking a picture and submitting.', style: TextStyle(fontSize: 15,color: Colors.black54,),textAlign: TextAlign.center,),
+                ),
+                MyTextField(
+                  controller: usernameController,
+                  hintText: 'OWA@nottingham.edu.my',
+                  obscureText: false,
+                ),
+                SizedBox(height: 10),
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 60,
+                  width: 300,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 3,
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0), // Adjust the radius as needed
+                      ),
+                    ),
+                    onPressed: (){signUserIn(context);},
+                    child: Text('Sign in', style: TextStyle(color: Colors.white, fontSize:20, ), textAlign: TextAlign.left,),
+                  ),
+                ),
+              Container(
+                margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+                child: Text(
+                'Dont have an account?',
+                style: TextStyle(
+                  color: Colors.black,
+                  ),
+                ),
+              ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      // Implement your registration logic here
+                      registerUser(context); // Close the dialog
+                    },
+                    child: Text(
+                      'Register now.',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+      toolbarHeight: 80,
+        title: Image.asset(
+        'assets/nottinghamlogoblack.png',
+        height: 200,
+        width: 250,
+        color: Colors.white,
+        colorBlendMode: BlendMode.darken,
+        fit: BoxFit.fitWidth,
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+    ),
       body: Stack(
         children: [
-          SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            left: 100,
+            bottom: 0,
+            child: Image.asset(
+              "assets/nottinghamlogo2.png",
+              color: Colors.white,
+              colorBlendMode: BlendMode.darken,
+            ),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: const SizedBox(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Spacer(flex: 1),
+              SizedBox(
+                width: 260,
                 child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 260,
-                    child: Column(
-                        children: const [
-                        Text(
-                            "Nott-a-problem",
-                            style: TextStyle(
-                            fontSize: 60,
-                            fontFamily: "Poppins",
-                            height: 1.2,
-                            ),
-                          ),
-                        SizedBox(height: 16),
-                        Text(
-                          "Here to solve your problems.",
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Spacer(flex: 2),
-                //Username TextField
-                  MyTextField(
-                  controller: usernameController,
-                  hintText:
-                      'Username(OWA)', // Change the hint for the username field
-                  obscureText: false,
-                ),
-
-                  const SizedBox(height: 20),
-
-                // Password TextField
-                  MyTextField(
-                  controller:
-                      passwordController, // Use the passwordController for the password field
-                  hintText:
-                      'Password', // Change the hint for the password field
-                  obscureText: true,
-                ),
-
-                  const SizedBox(height: 10),
-
-                  const SizedBox(height: 10),
-
-                // Sign in Button
-                  MyButton(
-                  onTap: signUserIn,
-                ),
-
-                  const SizedBox(height: 20), // Add some spacing here
-
-                //Not a member? Register Now
-                  Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+                    children: const [
                     Text(
-                      'Not a member?',
-                      style: TextStyle(color: Colors.grey[700]),
-                    ),
-                    const SizedBox(width: 4),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/registrationpage');
-                      },
-                      child: const Text(
-                        'Register now',
+                        "Nott-A-problem",
                         style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
+                        fontSize: 55,
+                        fontWeight: FontWeight.w700,
+                        // height: 1.2,
                         ),
                       ),
+                    SizedBox(height: 16),
+                    Text(
+                      "Don't skip out on the faulty facilities. Fix them by just a snap of a picture with Nott-A-Problem. Make our campus a better place.",
                     ),
                   ],
                 ),
-              ],
+              ),
+              const Spacer(flex: 1),
+              ClipRRect(
+                borderRadius: BorderRadius.horizontal(
+                  left: Radius.circular(25.0), // Adjust the radius as needed
+                  right: Radius.circular(25.0), // Adjust the radius as needed
+                ),
+                child: SizedBox(
+                  height: 60,
+                  width: 300,
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 3,
+                      backgroundColor: Colors.black, // Background color
+                    ),
+                    onPressed: () {
+                      showSignInDialog(context);
+                    },
+                    label: Text('Get Started', style: TextStyle(color: Colors.white, fontSize:20, ), textAlign: TextAlign.left,),
+                    icon: Icon(Icons.arrow_right_alt, color: Colors.white,),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                child: Text(
+                  "Signing up includes points to show off to your friends that you care about our campus facilities and earn rewards.",
+                ),
+              ),
+              const Spacer(flex: 1),
+          ],
             ),
-            ),
-          ),],
+            ),],
       ),
     );
   }
--- lib/pages/settingspage.dart
+++ lib/pages/settingspage.dart
@@ -12,12 +12,191 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-      ),
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Text('settings'),
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Text('Settings'),
+        backgroundColor: Colors.blue[100],
+        elevation: 0,
+      ),
+      body:
+      SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+              child: Card(
+                color: Colors.blue[50],
+                elevation: 15.0, // Add elevation for a shadow effect
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      16.0), // Adjust the border radius as needed
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Stack(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.black,
+                                      radius: 30,
+                                      child: Icon(
+                                        Icons.person_2_sharp,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ), //avatar
+                              Container(
+                                margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                                child: Text(
+                                  'Username: \n\nLevel: \n\nPoints:',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              )
+                              , //profile stats
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.description, size: 30),
+                    title: Text('Terms of Service', style: TextStyle(fontSize: 20,),),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Terms of Service'),
+                            content: Text('terms of service'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.question_mark, size: 30),
+                    title: Text('Help', style: TextStyle(fontSize: 20,),),
+                    onTap: () {
+                      // Implement the logic for the "Help" section
+                      // For example, you can navigate to a new page or show a dialog
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Help'),
+                            content: Text('help'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.edit, size: 30),
+                    title: Text('Edit Profile',style: TextStyle(fontSize: 20,),),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Edit Profile'),
+                            content: Text('Your edit profile content goes here.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                  Divider(), // A divider to separate sections
+                  ListTile(
+                    leading: Icon(Icons.logout, size: 30),
+                    title: Text('Logout', style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700, color: Colors.redAccent,),),
+                    onTap: () {
+                      // Implement the logic for the "Logout" section
+                      // For example, you can show a confirmation dialog
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Logout',),
+                            content: Text('Are you sure you want to logout?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  // Perform the logout action
+                                  // For example, you can navigate to the login page
+                                  Navigator.pushNamedAndRemoveUntil(
+                                    context,
+                                    '/loginpage',
+                                        (route) => false,
+                                  );
+                                },
+                                child: Text('Yes'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/loginpage');
+                                },
+                                child: Text('No'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
