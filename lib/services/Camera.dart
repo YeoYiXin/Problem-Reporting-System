ac2c834cde1a3844444f0936fa60994914f14366
ELVIS SIMIYU <91319107+Nerothenerd02@users.noreply.github.com>
2024-01-31 10:30:05 +0800

    WORKING ON THE API

--- lib/services/Camera.dart
+++ lib/services/Camera.dart
@@ -1,29 +1,57 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:flutter/material.dart';
-import 'package:problem_reporting_system/pages/ErrorIdentificationPage.dart';
+import 'package:problem_reporting_system/pages/resultDisplay.dart';
+import 'package:problem_reporting_system/pages/ImageClassificationAPI%20.dart';
+import 'package:http_parser/http_parser.dart';
 
 class Camera {
   File? imageFile;
   VoidCallback? onImageSelected;
 
-  void onTapCameraButton(BuildContext context) {
-    Future.delayed(Duration.zero, () {
-      getImage(context: context, source: ImageSource.camera);
+  Future<void> onTapCameraButton(BuildContext context) async {
+    await getImage(context: context, source: ImageSource.camera);
+  }
+
+  Future<void> getImage(
+      {required BuildContext context, required ImageSource source}) async {
+    ImagePicker().pickImage(source: source).then((file) {
+      if (file?.path != null) {
+        imageFile = File(file!.path);
+        onImageSelected?.call();
+        _processImage(context);
+      }
     });
   }
 
-  void getImage({required BuildContext context, required ImageSource source}) {
-    ImagePicker().pickImage(source: source).then((file) {
-      if (file?.path != null) {
-          imageFile = File(file!.path);
-          onImageSelected?.call();
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => ErrorIdentification(imageFile: imageFile),
-          ),
-        );
-      }
-    });
+  Future<void> _processImage(BuildContext context) async {
+    try {
+      // Convert File to MultipartFile
+      http.MultipartFile image = await http.MultipartFile.fromPath(
+        'file',
+        imageFile!.path,
+        contentType: new MediaType('image', 'jpeg'),
+      );
+
+      // Initialize the image classification API
+      ImageClassificationAPI api = ImageClassificationAPI(
+        'http://localhost:5000',
+      );
+
+      // Perform the image classification API call
+      String result = await api.getClass(image);
+
+      // Navigate to ResultDisplay page with the result
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => ResultDisplay(result: result),
+        ),
+      );
+    } catch (e) {
+      print('Error processing image: $e');
+      // Handle error (e.g., display an error message)
+    }
   }
 }
