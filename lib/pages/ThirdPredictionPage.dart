48c04e0e9e06af72a5dc9f59488cc6807d4d0a33
ELVIS SIMIYU <91319107+Nerothenerd02@users.noreply.github.com>
2024-03-04 16:30:59 +0800

    Some Frontend fixes

--- lib/pages/ThirdPredictionPage.dart
+++ lib/pages/ThirdPredictionPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:problem_reporting_system/pages/noEventDetected.dart';
 import 'package:problem_reporting_system/pages/problem_submission_database.dart';
 import 'submittedpage.dart';
+import 'package:problem_reporting_system/pages/appBackground.dart';
 
 class ThirdPredictionPage extends StatelessWidget {
   final File? imageFile;
@@ -22,108 +23,138 @@ class ThirdPredictionPage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        toolbarHeight: 80,
-        title: Text('Third Prediction'),
-        backgroundColor: Colors.blue[50],
-        elevation: 0,
-        automaticallyImplyLeading: false,
-      ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Container(
-                  width: 300,
-                  height: 300,
-                  child: imageFile != null
-                      ? Image.file(
-                          imageFile!) // Add ! to access non-nullable File
-                      : const Text('Image not available'),
-                ),
-              ),
-              SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  'Third Prediction Results:',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
+        child: Stack(
+          children: [
+            appBackground(),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 40),
+                  Center(
+                    child: Text(
+                      'Nott-A-Problem',
+                      style: TextStyle(
+                        fontFamily: 'Lobster',
+                        fontSize: 50,
+                        color: Colors.black,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: thirdPredictionResult.isNotEmpty
-                    ? Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                  SizedBox(height: 20),
+                  Center(
+                    child: Container(
+                      width: 300,
+                      height: 300,
+                      child: imageFile != null
+                          ? Image.file(imageFile!)
+                          : const Text('Image not available'),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Card(
+                    margin: EdgeInsets.all(16.0),
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
                         children: [
-                          Text(
-                            'Class: ${thirdPredictionResult[0].replaceAll('_', ' ')}',
-                            style: TextStyle(fontSize: 20.0),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'Subclass Prediction Results:',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                          Text(
-                            'Subclass: ${thirdPredictionResult[1]}',
-                            style: TextStyle(fontSize: 20.0),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: thirdPredictionResult.isNotEmpty
+                                ? Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Class: ${thirdPredictionResult[0].replaceAll('_', ' ')}',
+                                        style: TextStyle(fontSize: 20.0),
+                                      ),
+                                      Text(
+                                        'Subclass: ${thirdPredictionResult[1]}',
+                                        style: TextStyle(fontSize: 20.0),
+                                      ),
+                                    ],
+                                  )
+                                : Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Text(
+                                      'The location is $locationInfo',
+                                      style: TextStyle(fontSize: 20.0),
+                                    ),
+                                  ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Is this correct?',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (thirdPredictionResult[0]
+                                          .replaceAll('_', ' ')
+                                          .toLowerCase() ==
+                                      'no event') {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                NoEventThankYou()));
+                                  } else {
+                                    Problem_Submission_Database()
+                                        .recordProblemSubmission(
+                                      pIndoorLocation: roomNumber,
+                                      titleClass: thirdPredictionResult[0]
+                                          .replaceAll('_', ' '),
+                                      subClass: thirdPredictionResult[1],
+                                      description: '', //empty
+                                      location: locationInfo,
+                                      imageURL: imageFile!,
+                                      userTyped: false,
+                                    );
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) => Submitted()));
+                                  }
+                                },
+                                child: Text('Yes'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  _showDescriptionDialog(context);
+                                },
+                                child: Text('No'),
+                              ),
+                            ],
                           ),
                         ],
-                      )
-                    : Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          'The location is  $locationInfo',
-                          style: TextStyle(fontSize: 20.0),
-                        ),
                       ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Is this correct?',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (thirdPredictionResult[0]
-                              .replaceAll('_', ' ')
-                              .toLowerCase() ==
-                          'no event') {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => NoEventThankYou()));
-                      } else {
-                        Problem_Submission_Database().recordProblemSubmission(
-                          pIndoorLocation: roomNumber,
-                          titleClass:
-                              thirdPredictionResult[0].replaceAll('_', ' '),
-                          subClass: thirdPredictionResult[1],
-                          description: '', //empty
-                          location: locationInfo,
-                          imageURL: imageFile!,
-                          userTyped: false,
-                        );
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => Submitted()));
-                      }
-                    },
-                    child: Text('Yes'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      _showDescriptionDialog(context);
-                    },
-                    child: Text('No'),
+                    ),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
