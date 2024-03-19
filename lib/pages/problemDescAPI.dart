7bbf09f0733691bacc51994b5e3a751a5a6adef6
YeoYiXin <sara.yeo.yixin@gmail.com>
2024-03-19 14:53:27 +0800

    add logo and name

--- lib/pages/problemDescAPI.dart
+++ lib/pages/problemDescAPI.dart
@@ -1,31 +1,14 @@
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
-import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 Future<String> getDescription(String imageURL) async {
   try {
-    // Download the image
-    // final response = await http.get(Uri.parse(imageURL));
-
-    // if (response.statusCode == 200) {
-    //   // Convert the response body to base64
-    //   String base64Image = base64Encode(response.bodyBytes);
-    //   print('base64Image: $base64Image');
     final response = await http.get(Uri.parse(imageURL));
 
     if (response.statusCode == 200) {
       // Convert the response body to bytes
       Uint8List bytes = response.bodyBytes;
-
-      // Resize the image to 512x512
-      // List<int> compressedBytes = await FlutterImageCompress.compressWithList(
-      //   bytes,
-      //   minHeight: 512,
-      //   minWidth: 512,
-      //   quality: 100,
-      // );
-
       // Convert bytes to base64
       String base64Image = base64Encode(bytes);
       print('base64Image: $base64Image');
@@ -49,14 +32,6 @@ Future<String> getDescription(String imageURL) async {
           print('Failed to parse JSON: $e');
           return 'Failed to parse JSON: $e';
         }
-        // final data = jsonDecode(response.body);
-
-        // return data.toString();
-        // if (data.containsKey('description')) {
-        //   return data['description'].toString();
-        // } else {
-        //   return 'Description not found in response';
-        // }
       } else {
         print('Server responded with status code ${apiResponse.statusCode}.');
         return 'Server responded with status code ${apiResponse.statusCode}.';
